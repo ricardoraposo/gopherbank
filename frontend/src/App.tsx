39797415cf891b1f6038e53dd0a502f905740e9b
Ricardo Raposo <ricardorbxx1@gmail.com>
2023-12-15 17:55:33 -0300

    removed the css files 🤮

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Initial from './pages/Initial';
 import Counter from './pages/Counter';
-import './App.css';
 
 function App() {
   return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
