efd2a5c139a318a57c17b9ab399a6f6f1ff06875
ricardo <ricardorbxx1@gmail.com>
2024-01-03 21:18:50 -0300

    added some animation to notification out

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { useAtom } from 'jotai';
+import { useState } from 'react';
 import Cancel from '../assets/cancel.svg';
 import { apiURL, queryParams } from '../consts';
 import { tokenAtom } from '../store/atom';
+import { sleep } from '../utils/helpers';
 
 type Props = {
   id: number;
@@ -13,6 +15,7 @@ type Props = {
 
 function Notification({ id, title, content }: Props) {
   const [token] = useAtom(tokenAtom);
+  const [shrink, setShrink] = useState(false);
   const queryClient = useQueryClient();
   const notification = useMutation({
     mutationFn: (id: number) => axios.delete(`${apiURL}/api/notification/${id}`, queryParams(token)),
@@ -21,13 +24,22 @@ function Notification({ id, title, content }: Props) {
     },
   });
 
+  const handleClick = async (id: number) => {
+    setShrink(true);
+    await sleep(500);
+    notification.mutate(id);
+  };
+
   return (
-    <div className="px-6 py-2 flex justify-center items-center bg-white">
+    <div
+      className={ `px-6 py-2 flex justify-center items-center bg-white origin-top
+                  transition-all duration-700 ${shrink ? 'scale-0' : ''}` }
+    >
       <div className="flex flex-col items-start">
         <p className="text-gray-400 text-base font-semibold">{title}</p>
         <p className="text-gray-800 text-xs text-start">{content}</p>
       </div>
-      <button onClick={ () => notification.mutate(id) }>
+      <button onClick={ () => handleClick(id) }>
         <img src={ Cancel } alt="clean notification" />
       </button>
     </div>
