7de53263b29c2a86ce8ab99ad0fe348b6e66c81d
ricardo <ricardorbxx1@gmail.com>
2024-01-29 20:51:54 -0300

    feat: added graceful shutdown to application

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/ricardoraposo/gopherbank/internal/server"
@@ -12,8 +16,23 @@ func main() {
 	app := server.New()
 	app.RegisterRoutes()
 
-    port := fmt.Sprintf("0.0.0.0:%s", os.Getenv("APP_PORT"))
-	if err := app.Listen(port); err != nil {
+	port := fmt.Sprintf("0.0.0.0:%s", os.Getenv("APP_PORT"))
+	go func() {
+		if err := app.Listen(port); err != nil {
+			panic(err)
+		}
+	}()
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
 		panic(err)
 	}
+
+	fmt.Println("\nServer gracefully stopped")
 }
