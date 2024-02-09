ec62ee823ace656abbae1bf6cf2f470bf51622a5
ricardo <ricardorbxx1@gmail.com>
2024-02-09 07:22:31 -0300

    feat: switched app protocol to read env variable instead of hard coded value

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,4 +1,4 @@
-export const apiURL = `http://${import.meta.env.VITE_API_HOST}`;
+export const apiURL = `${import.meta.env.VITE_API_PROTOCOL}://${import.meta.env.VITE_API_HOST}`;
 
 export const defaultPic = 'https://i.imgur.com/fLjGgnc.png';
 
